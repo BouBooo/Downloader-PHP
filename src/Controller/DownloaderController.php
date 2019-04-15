4863f0ab255d86cc4af91daf6892792da051f844
Florent Nicolas <flonicolas.pro@outlook.fr>
2019-04-15 12:45:37 +0200

    login/signin & Youtube track treatment

--- src/Controller/DownloaderController.php
+++ src/Controller/DownloaderController.php
@@ -10,7 +10,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class DownloaderController extends AbstractController
 {
     /**
-     * @Route("/", name="downloader")
+     * @Route("/", name="home")
+     */
+    public function home() {
+        return $this->render('home.html.twig');
+    }
+
+
+
+    /**
+     * @Route("/downloader", name="downloader")
      */
     public function index(Request $request)
     {   
@@ -23,9 +32,51 @@ class DownloaderController extends AbstractController
 
         if($YoutubeForm->isSubmitted() && $YoutubeForm->isValid()) {
 
-            return $this->render('downloader/youtube.html.twig', [
-                'result' => $YoutubeForm['youtube_link']->getData()
-            ]);
+            $url = $YoutubeForm['youtube_link']->getData();
+            $regex_pattern = "/(youtube.com|youtu.be)\/(watch)?(\?v=)?(\S+)?/";
+            $match;
+    
+            // Check valid Youtube url
+            if(preg_match($regex_pattern, $url, $match)) {
+                parse_str( parse_url($url, PHP_URL_QUERY ), $my_array_of_vars );
+                $api_key = 'AIzaSyCUCIPiVy6t0KigYdr9LgwkK55kWuUywxQ';
+
+                if(!empty($my_array_of_vars['list']))
+                {
+                    $video = '<iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list='.$my_array_of_vars['list'].'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+                    $download_link = '<span class="alert alert-danger"> Youtube playlist download don\'t work yet </span><br>';
+                }
+                // Youtube track
+                else if(empty($my_array_of_vars['list']) && !empty($my_array_of_vars['v']))
+                {               
+                    $video = $my_array_of_vars['v'];
+
+                    return $this->render('downloader/youtube.html.twig', [
+                        'video' => $video,
+                        'result' => $url,
+                        'api_key' => $api_key
+                    ]);
+
+                }
+                else
+                {
+                    // Get Channel Id only from Ytb URL
+                    $parts = parse_url($url);
+                    $path_parts= explode('/', $parts['path']);
+                    $channelId = $path_parts[2];
+                    $maxResults = 30;
+
+                    $videoList = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$channelId.'&maxResults='.$maxResults.'&key='.$api_key.''));
+
+                    $listVideoChannel = true;
+                }   
+            }
+            else {
+                return $this->render('downloader/youtube.html.twig', [
+                    'result' => 'Invalid url',
+                    'api_key' => 'Not delivered yet'
+                ]);
+            }
             //return $this->redirectToRoute('downloader/youtube.html.twig');
         }
 
---
+++ src/Entity/User.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ * fields = {"email"},
+ * message = "Email already used"
+ * )
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="8", minMessage="Your password must be at least 8 characters long")
+     */
+    private $password;
+
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="Your passwords doesn't match")
+     *
+     */
+    private $confirmPassword;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getConfirmPassword(): ?string
+    {
+        return $this->confirmPassword;
+    }
+
+    public function setConfirmPassword(string $confirmPassword): self
+    {
+        $this->confirmPassword = $confirmPassword;
+
+        return $this;
+    }
+
+    //Implements
+
+    public function eraseCredentials() {}
+
+    public function getSalt() {}
+
+    public function getRoles() {
+        return ['ROLES_USER'];
+    }
+}
